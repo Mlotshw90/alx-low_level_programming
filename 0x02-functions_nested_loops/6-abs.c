7e7e4eb3ae9a0b936f06b934704d0007b5e54b21
Dineo <mlotshwadineo080@gmail.com>
2023-02-20 23:48:56 -0800

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,11 @@
+/**
+ * _abs - computes the absolute value of an integer
+ * @a: number whose absolute value is to be found
+ * Reurn: Absolute value of the number
+ */
+int _abs(int a)
+{
+	if (a < 0)
+		a *= -1;
+	return (a);
+}
