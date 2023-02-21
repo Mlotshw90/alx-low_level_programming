013bd08c3fb3333d5a5cb89568f6243f72369d6f
Dineo <mlotshwadineo080@gmail.com>
2023-02-20 23:25:33 -0800

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _islower - checks whether character passed is lowercase
+ * @c: character to be tested
+ * Return: 1 if @c islowercase otherwise 0
+ */
+int _islower(int c)
+{
+	if (c >= 97 && 122)
+		return (1);
+	else
+		return (0);
+}
